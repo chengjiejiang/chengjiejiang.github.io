6a7662f15833ab5c82705a5eb1e0dd29fd1d342b
chengjiejiang <lawrencejcj@outlook.com>
2025-08-03 21:45:07 +0000

    Deploying to gh-pages from @ chengjiejiang/chengjiejiang.github.io@f0a24b5333291ffd444e9c1833bef49b6d30c60c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -11,7 +11,7 @@ ninja.data = [{
     },
   },{id: "nav-publications",
           title: "Publications",
-          description: "",
+          description: "Peer-reviewed journal articles",
           section: "Navigation",
           handler: () => {
             window.location.href = "/publications/";
