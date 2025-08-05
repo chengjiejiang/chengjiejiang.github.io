0b08d3330baf72d0ef872bba8a64149f891f82e5
chengjiejiang <lawrencejcj@outlook.com>
2025-08-05 19:25:35 +0000

    Deploying to gh-pages from @ chengjiejiang/chengjiejiang.github.io@dcf9f1d53cdf4d208d4169f9c86663aa3c7a8fbf 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -32,7 +32,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "CV",
-          description: "Last updated in April 2025",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
